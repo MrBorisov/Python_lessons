0f2aa9736fb85ffbeec874b6f86f886ddbdf281d
mrborisov <mrborisovv@gmail.com>
2020-04-04 11:30:00 +1000

    optimal

--- 2.py
+++ 2.py
@@ -1,20 +1,7 @@
 seconds = int(input('Введите количество секунд: '))
 
 hours = seconds // 3600
-
-if hours < 10:
-    hours = f'0{hours}'
-else:
-    hours = f'{hours}'
-
 minutes = (seconds - int(hours) * 3600) // 60
-
-if minutes < 10:
-    minutes = f'0{minutes}'
-
 seconds = seconds - int(hours) * 3600 - int(minutes) * 60
-if seconds < 10:
-    seconds = f'0{seconds}'
-
-print(f'{hours}:{minutes}:{seconds}')
+print(f'{hours:02}:{minutes:02}:{seconds:02}')
 
--- 6.py
+++ 6.py
@@ -2,6 +2,6 @@ a = int(input('введите результат первого дня: '))
 b = int(input('введите цель: '))
 day = 1
 while a < b:
-    a = a + a * 10 / 100
+    a = a + a * 0.1
     day += 1
 print(day)
