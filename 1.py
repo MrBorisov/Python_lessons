ee2ab518834fa47cc65bf373d37d5e60db36bedb
mrborisov <mrborisovv@gmail.com>
2020-04-04 14:32:01 +1000

    first task

---
+++ 1.py
@@ -0,0 +1,4 @@
+my_list = [16, 5.2, 3+5j, 'text', False, None, (1,2,3), ['a', 'b'], set('barabekus'), frozenset('mrborisov'),
+           {"first": 1, 2: 'second', 3: None}, bytes(10)]
+for element in my_list:
+    print(type(element))
