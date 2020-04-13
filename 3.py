0aabc93cc0ff22d52419fdfb5aee2b6e4baedbee
mrborisov <mrborisovv@gmail.com>
2020-04-14 09:48:59 +1000

    3 changed

--- 3.py
+++ 3.py
@@ -1,10 +1,10 @@
 """Сложно отладить отлаживать такие решения."""
-my_list = [i for i in range(20, 240) if not i%20 or not i%21]
+my_list = [i for i in range(20, 241) if not i%20 or not i%21]
 print(my_list)
 
 """Преаерка"""
 my_list = []
-for i in range(20, 240):
+for i in range(20, 241):
 
     if not i % 20 or not i % 21:
         my_list.append(i)
