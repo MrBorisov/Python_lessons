fe9408be7b101f7b96c9b38bfe6d0ba54b5958b3
mrborisov <mrborisovv@gmail.com>
2020-04-19 11:08:39 +1000

    1 and 2 changed

--- 1.py
+++ 1.py
@@ -3,7 +3,8 @@ try:
         while True:
             data = input('Введите строку, для завершения не вводите ни чего :')
             if data:
-                file.write(data + '\n')
+                #file.write(data + '\n')
+                print(data, file=file)
             else:
                 break
 except IOError:
--- 2.py
+++ 2.py
@@ -9,3 +9,10 @@ try:
         print(f'Всего строк {num_of_str}, а слов {num_of_words}')
 except IOError:
     print("Произошла ошибка ввода-вывода!")
+
+#-----------------------------вариант 2----------------
+with open("task_2.txt") as file:
+    my_line = file.readlines()
+    for index, value in enumerate(my_line):
+        num_of_words = len(value.split())
+        print('Строка {} содержит {} слов'.format(index + 1, num_of_words))
